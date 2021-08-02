9394e908250c7cd2c8c49b5636a46529b7d9f6c1
Yaroslav Didyk <slavan.didyk@gmail.com>
2021-08-02 18:42:09 +0300

    Finished programming

--- simple-calculator/app.js
+++ simple-calculator/app.js
@@ -1,6 +1,29 @@
 let input = document.querySelector('input')
 
-// input numbers to the area
+// Input numbers to the area
 function insert(ch) {
   input.value += ch
+}
+
+// Clear input
+function clearInput() {
+  input.value = ""
+}
+
+// Delete last num
+function deleteArrow() {
+  let target = input.value
+  input.value = target.substring(0, target.length - 1)
+}
+
+// Result
+function result() {
+  let target = input.value
+
+  if (target) {
+    input.value = eval(target)
+  }
+  else {
+    alert("Please, fill in the blank")
+  }
 }
