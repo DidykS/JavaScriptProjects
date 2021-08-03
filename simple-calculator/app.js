d8001c6ad08aade4709cb72cad996751f0f8895e
Yaroslav Didyk <slavan.didyk@gmail.com>
2021-08-03 21:12:03 +0300

    Rewrote the comments to the function.

--- simple-calculator/app.js
+++ simple-calculator/app.js
@@ -1,22 +1,26 @@
 let input = document.querySelector('input')
 
-// Input numbers to the area
+// Input numbers to the area:
+// In this function, as a parameter, I accept the character which is entered in an input field.
 function insert(ch) {
   input.value += ch
 }
 
-// Clear input
+// Clear input:
+// In this function, I clear the field for entering numbers.
 function clearInput() {
   input.value = ""
 }
 
-// Delete last num
+// Delete last num:
+// This function removes the last character in the number entry field.
 function deleteArrow() {
   let target = input.value
   input.value = target.substring(0, target.length - 1)
 }
 
 // Result
+// In this function, I use the built-in function eval (). The eval function executes the code presented as a string.
 function result() {
   let target = input.value
 
