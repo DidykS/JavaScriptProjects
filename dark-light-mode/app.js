73825c7d252d8f5c58c3cdf9dd37c88eb07bbbac
Yaroslav Didyk <slavan.didyk@gmail.com>
2021-08-07 11:29:39 +0300

    I add comments to the js file, change the logo and background.

--- dark-light-mode/app.js
+++ dark-light-mode/app.js
@@ -3,6 +3,7 @@ let ball = document.querySelector(".ball")
 let container = document.querySelector(".container")
 let light = document.querySelector(".light")
 
+// In this click function, I change from default styles to dark styles
 switcher.addEventListener("click", function() {
   ball.classList.toggle("check")
   container.classList.toggle("dark")
