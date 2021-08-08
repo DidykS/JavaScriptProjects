51118914f6e9d48a3b16b7cdde548892a0fc391e
Yaroslav Didyk <slavan.didyk@gmail.com>
2021-08-08 15:45:24 +0300

    I output users to the page through the fetch method.

---
+++ live-search/app.js
@@ -0,0 +1,29 @@
+const input = document.querySelector("#search") // input 
+const result = document.querySelector("#users") // ul
+
+const userList = []
+
+// Method fetch()
+async function getUsers() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const data = await response.json()
+  toHtml(data)
+}
+getUsers()
+
+function toHtml(data) {
+  data.map(item => {
+    let li = document.createElement("li")
+    userList.push(li)
+
+    li.innerHTML = `
+      <div class="users__info">
+        <h4> ${item.name} </h4>
+        <p> ${item.username} </p>
+      </div>  
+    `
+
+    result.appendChild(li)
+  })
+}
+
