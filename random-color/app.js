be5d19d1fe801c150039fce979e4eb3235bd6f97
Yaroslav Didyk <slavan.didyk@gmail.com>
2021-08-12 13:44:15 +0300

    Create a clipboard function.

--- random-color/app.js
+++ random-color/app.js
@@ -1,7 +1,9 @@
 let btn = document.getElementById("btn")
 let text = document.getElementById("text")
+let copy = document.querySelector("#copy")
 
 btn.addEventListener("click", getRandom)
+copy.addEventListener("click", copyText)
 
 /* getRandom function
   In this function, I take random colors from the method, 
@@ -11,4 +13,13 @@ function getRandom() {
   let randomColor = Math.floor(Math.random() * 16777216).toString(16)
   document.body.style.background = "#" + randomColor
   text.innerHTML = "#" + randomColor
+  copy.innerText = "Copy"
+}
+
+/* copyText function 
+  This function sends the text to the clipboard on click on the block.
+*/
+function copyText() {
+  navigator.clipboard.writeText(text.innerText)
+  copy.innerText = "Copied"
 }
